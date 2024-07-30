6f17cd39ca8aaebf2b6456f1653460c22741d5e7
Sujitha-5801 <146907319+Sujitha-5801@users.noreply.github.com>
2024-07-31 05:16:12 +0530

    Update Light.java

--- DesignPrinciplesAndPatterns/CommandPattern/Light.java
+++ DesignPrinciplesAndPatterns/CommandPattern/Light.java
@@ -1,1 +1,11 @@
+package CommandPatternExample;
 
+public class Light {
+    public void turnOn() {
+        System.out.println("The light is on.");
+    }
+
+    public void turnOff() {
+        System.out.println("The light is off.");
+    }
+}
