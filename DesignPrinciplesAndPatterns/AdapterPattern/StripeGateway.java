70001767f872e8948cb4d05c71c8f5112bd04e96
Sujitha-5801 <146907319+Sujitha-5801@users.noreply.github.com>
2024-07-31 05:13:10 +0530

    Update StripeGateway.java

--- DesignPrinciplesAndPatterns/AdapterPattern/StripeGateway.java
+++ DesignPrinciplesAndPatterns/AdapterPattern/StripeGateway.java
@@ -1,1 +1,8 @@
+package AdapterPatternExample;
 
+// StripeGateway.java
+public class StripeGateway {
+    public void pay(double amount) {
+        System.out.println("Processing payment of $" + amount + " through Stripe.");
+    }
+}
